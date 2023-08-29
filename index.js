4da9633f4f938834b44f28286c85ef230e845941
+ <+>
2023-08-28 17:44:25 -0700

    loops

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names){
+const thankYou = [];
+for(let i=0; i < names.length; i++){
+    thankYou.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+}
+return thankYou;
+}
+
+function countDown(startNum) {
+    while(startNum>=0) {
+        console.log(startNum);
+        startNum--;
+    }
+}
